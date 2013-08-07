17054b6b63eaf3347228b6404172dfcb6e391b3b
Lisnichenko Dmitro <dlisnichenko@cybervisiontech.com>
2013-08-07 17:32:23 +0300

    box import script

---
+++ scripts/vbox_to_vagrant.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ $# -ne 2 ]
+then
+  echo -e "Syntax:\n$0 <source virtualbox vm name> <target vagrant box name>\nList of accessible VBox vms:"
+  vboxmanage list vms
+else
+  SRC_NAME=$1
+  TGT_NAME=$2
+
+  vagrant package --base "$SRC_NAME"
+  vagrant box add "$TGT_NAME" package.box
+  rm package.box
+fi
